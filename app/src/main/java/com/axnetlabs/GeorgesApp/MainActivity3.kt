7e8e85ead513616aa48c500510c48a60a26e0da5
georgeshea <52836004+GeorgeJShea@users.noreply.github.com>
2022-10-29 12:51:17 -0500

    added many an activity

--- app/src/main/java/com/axnetlabs/GeorgesApp/MainActivity3.kt
+++ app/src/main/java/com/axnetlabs/GeorgesApp/MainActivity3.kt
@@ -3,9 +3,9 @@ package com.axnetlabs.phpcrudecrud
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
+        setContentView(R.layout.activity_main6)
     }
 }
